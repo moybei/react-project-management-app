a74e2a633e41da5009e75ea0339223b5393bcebc
Moy Haobei <moybei1999@gmail.com>
2024-08-11 17:09:39 +0800

    Added basic components for adding projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
+import { useState } from "react";
+
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      };
+    })
+  }
+
+  function handleAddProject(projectData) {
+    const projectId = Math.random();
+
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: projectId
+      }
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      };
+    })
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAddProject={handleAddProject} />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectState.projects}/>
+      {content}
+    </main>
   );
 }
 
